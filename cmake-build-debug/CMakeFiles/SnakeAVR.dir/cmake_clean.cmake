93ab24ddbc3ccd48364a036e540d4a5a40ca1912
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2021-08-07 15:53:31 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/SnakeAVR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SnakeAVR.dir/main.cpp.o"
+  "SnakeAVR"
+  "SnakeAVR.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SnakeAVR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SnakeAVR.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SnakeAVR.
+# This may be replaced when dependencies are built.
